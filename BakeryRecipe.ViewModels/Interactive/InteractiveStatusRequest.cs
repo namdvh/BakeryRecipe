37038c28dfeadafbd9d9df9d741de1b0c1b9db22
Thinh Nguyen <anhthinh.61.got@gmail.com>
2022-10-04 10:49:45 +0700

    updated interactive status

---
+++ BakeryRecipe.ViewModels/Interactive/InteractiveStatusRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BakeryRecipe.ViewModels.Interactive
+{
+    public class InteractiveStatusRequest
+    {
+        public bool isInteractive { get; set; }
+
+        public bool? isLike { get; set; }
+
+        public bool? isDislike { get; set; }
+    }
+}
